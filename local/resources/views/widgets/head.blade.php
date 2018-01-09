f2f3acbad4acccceae795c8a4ec7a867ae1cb110
minhminh1502 <minhm9986@gmail.com>
2018-01-09 08:53:35 +0700

    load post

--- local/resources/views/widgets/head.blade.php
+++ local/resources/views/widgets/head.blade.php
@@ -36,3 +36,21 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	});
 </script>
 <!-- start-smoth-scrolling -->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '379239492535415',
+      xfbml      : true,
+      version    : 'v2.11'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
